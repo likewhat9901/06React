dcd4dc96ce6420d0b98d6dbb53d9e7d7c37b00cc
likewhat9901 <parkko123@naver.com>
2025-05-14 18:26:46 +0900

    250514

--- react01-basic/src/main.jsx
+++ react01-basic/src/main.jsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
