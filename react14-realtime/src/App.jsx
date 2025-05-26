afc68a4d51a1478377d7eb1ef56232825bdec570
likewhat9901 <parkko123@naver.com>
2025-05-26 17:32:52 +0900

    1234

--- react14-realtime/src/App.jsx
+++ react14-realtime/src/App.jsx
@@ -2,9 +2,9 @@ import { realtime } from "./realtimeConfig";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 import RealtimeCRUD from "./components/RealtimeCRUD";
-// import Listener from "./components/Listener";
-// import ChatStart from "./components/ChatStart";
-// import ChatMessage from "./components/ChatMessage";
+import Listener from "./components/Listener";
+import ChatStart from "./components/ChatStart";
+import ChatMessage from "./components/ChatMessage";
 
 function App() {
   console.log("realtime", realtime);
@@ -14,11 +14,14 @@ function App() {
         <Routes>
           <Route path="/" element={<RealtimeCRUD/>}/>
           <Route path="/crud" element={<RealtimeCRUD/>}/>
-          {/* <Route path="/listener" element={<Listener/>}/> */}
-          {/* <Route path='/chat' element={<Outlet/>}>
+          <Route path="/listener" element={<Listener/>}/>
+          {/* 실시간 채팅은 2단계 라우팅 처리가 되어있다.
+          첫번째 화면은 대화방, 대화명 입력을 위한 입력상자가 있음.
+          2개의 정보를 입력 후 팝업창으로 채팅 대화창(2번째 화면)을 띄우게 됨. */}
+          <Route path='/chat' element={<Outlet/>}>
             <Route index element={<ChatStart />}/>
             <Route path="talk" element={<ChatMessage />}/>
-          </Route> */}
+          </Route>
         </Routes>
       {/* </div> */}
     </BrowserRouter>
